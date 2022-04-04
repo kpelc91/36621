e62546d2547a3bdcd18e410ec92e40582fe193bf
kpelc <kamilapelc01@gmail.com>
2022-04-04 16:11:43 +0200

    zadanie zrobione

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let name1 = 'firstNumber';
+
+let number1 = 5;
+
+let name2 = 'secondNumber';
+
+let number2 = 10;
+
+function sum(number1, number2) {
+    let result = number1 + number2;
+
+    return result;
+}
+
+console.log( sum(number1, number2));
